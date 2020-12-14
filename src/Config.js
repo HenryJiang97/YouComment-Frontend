7ab7a388e6cd1574fd727fe0828331348ab3b16b
wang.xin10 <wang.xin10@husky.neu.edu>
2020-12-13 23:17:59 -0800

    add PWA

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
-// const commentApiPrefix = "https://you-comment-backend.herokuapp.com/api/comment/";
+ //const commentApiPrefix = "https://you-comment-backend.herokuapp.com/api/comment/";
 const commentApiPrefix = "http://localhost:3000/api/comment/";
-// const userApiPrefix = "https://you-comment-backend.herokuapp.com/api/user/";
+ //const userApiPrefix = "https://you-comment-backend.herokuapp.com/api/user/";
 const userApiPrefix = "http://localhost:3000/api/user/";
 const youtubeApiKey = "AIzaSyCB0KooQ52eIZKR_kieRhEbrcGvbfBH-lg";
 const youtubeBaseUrl = "https://www.googleapis.com/youtube/v3/search";
